71a4773075271163ac35ab52874857d8e00b7976
CAWI\User <cawicaancornelio@gmail.com>
2020-02-06 18:46:08 +1300

    Initial commit

---
+++ Lychee.Domain/Lychee.Domain/Interfaces/ISettingRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Lychee.Entities.Entities;
+
+namespace Lychee.Domain.Interfaces
+{
+    public interface ISettingRepository
+    {
+        ICollection<Setting> GetAllSettings();
+        Setting GetSetting(string key);
+        T GetSettingValue<T>(string key);
+    }
+}
